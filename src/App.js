d511d8087fbd7e399a78f01411edac21b5b1b137
GA20182499 <gk22@apple.com>
2022-06-09 02:51:35 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/Home";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import { Suspense, lazy } from 'react';
+import NavBar from "./components/layout/NavBar";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import NotFound from "./components/pages/NotFound";
+import AddIssue from "./components/users/AddIssue";
+import UpdateIssue from "./components/users/UpdateIssue";
+import ViewForm from "./components/users/ViewForm";
+import Profile from "./components/layout/Profile"
+const Chart = lazy(() => import('./components/pages/Chart'));
+const About=lazy(()=>import('./components/pages/About'));
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/issues" component={Home} />
+          <Route exact path="/users/add" component={AddIssue} />
+          <Route exact path="/users/edit/:id" component={UpdateIssue} />
+          <Route exact path="/viewissues/:id" component={ViewForm} />
+          <Route exact path="/profile" component={Profile}/>
+          <Route path="/chart" 
+                render={() => (<Suspense fallback={<h1>Loading...</h1>}><Chart/></Suspense>)}
+              />
+              <Route path="/about" 
+                render={() => (<Suspense fallback={<h1>Loading...</h1>}><About/></Suspense>)}
+              />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
